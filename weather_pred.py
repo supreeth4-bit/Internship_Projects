82c0239312cc1dda0ba0de46cf4a743d1b8374e6
supreeth4-bit <supreeth340@gmail.com>
2026-03-20 11:51:07 +0530

    Update weather_pred.py

--- weather_pred.py
+++ weather_pred.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, r2_score
 import matplotlib.pyplot as plt
-
+#For this download the given csv file in the repository
 #Load weather dataset
 #For other datasets,replace 'weather_data.csv' with your file name
 #CSV should have columns:Date,Temperature
